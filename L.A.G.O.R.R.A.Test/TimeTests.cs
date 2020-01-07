9e4b146a7f95b50c3276bc10c986cc64cf52fcbb
juanmafc <juanmafc25@gmail.com>
2020-01-07 20:38:02 -0300

    Time test added

---
+++ L.A.G.O.R.R.A.Test/TimeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace L.A.G.O.R.R.A.Test
+{
+    [TestFixture]
+    public class TimeTests
+    {
+        [Test]
+        public void GivenTwoTimes_WhenGettingTimeDifference_TheAbsoluteDifferenceInHoursAndMinutesIsReturned()
+        {
+            Time time1 = new Time("19:00:00");
+            Time time2 = new Time("7:00:00");
+            
+            Assert.AreEqual("12:00", time1.getTimeDifference(time2));
+            Assert.AreEqual("12:00", time2.getTimeDifference(time1));
+        }
+    }
+}
