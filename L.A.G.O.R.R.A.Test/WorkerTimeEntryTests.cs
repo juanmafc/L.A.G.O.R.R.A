3640bc7502b03577610dc6e1fa2cf0b45d119caa
juanmafc <juanmafc25@gmail.com>
2020-01-07 23:39:41 -0300

    WorkerTimeEntryTests added

---
+++ L.A.G.O.R.R.A.Test/WorkerTimeEntryTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace L.A.G.O.R.R.A.Test
+{
+    [TestFixture]
+    public class WorkerTimeEntryTests
+    {
+
+        private DateTime CreateEntryTimestamp(int hour, int minutes)
+        {
+            return new DateTime(2020, 12, 25, hour, minutes, 0);
+        }
+        
+        private WorkerTimeEntry CreateWorkerTimeEntry(DateTime entryTimestamp)
+        {
+            const int workerId = 0;
+            return new WorkerTimeEntry(workerId, entryTimestamp);
+        }
+        
+        public WorkerTimeEntryTestingClass GivenAWorkerTimeEntryAt(int hour, int minutes)
+        {
+            var entryTimestamp = CreateEntryTimestamp(hour, minutes);
+            var entry = CreateWorkerTimeEntry(entryTimestamp);
+            return new WorkerTimeEntryTestingClass(entry);
+        }
+
+        public class WorkerTimeEntryTestingClass
+        {
+            private WorkerTimeEntry entry;
+
+            public WorkerTimeEntryTestingClass(WorkerTimeEntry entry)
+            {
+                this.entry = entry;
+            }
+
+
+            public void TheRoundedTimeShouldBe(string roundedTime)
+            {
+                Assert.AreEqual(roundedTime, entry.getRoundedTime());
+            }
+        }
+        
+        
+        [Test]
+        public void GivenATimeEntry_WhenRoundingTime_ThenTheExpectedRoundedTimeShouldBeReturned()
+        {
+            GivenAWorkerTimeEntryAt(7, 0).TheRoundedTimeShouldBe("7:00");
+            GivenAWorkerTimeEntryAt(7, 1).TheRoundedTimeShouldBe("7:00");
+            GivenAWorkerTimeEntryAt(7, 14).TheRoundedTimeShouldBe("7:00");
+            
+            GivenAWorkerTimeEntryAt(7, 15).TheRoundedTimeShouldBe("7:30");
+            GivenAWorkerTimeEntryAt(7, 44).TheRoundedTimeShouldBe("7:30");
+            GivenAWorkerTimeEntryAt(7, 45).TheRoundedTimeShouldBe("7:30");
+            
+            GivenAWorkerTimeEntryAt(7, 46).TheRoundedTimeShouldBe("8:00");
+            GivenAWorkerTimeEntryAt(7, 59).TheRoundedTimeShouldBe("8:00");
+            GivenAWorkerTimeEntryAt(8, 0).TheRoundedTimeShouldBe("8:00");
+        }
+    }
+}
